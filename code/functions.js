f378b926c14ebf6834c3c86fe3af073eee68a4d3
EWAN <ewanritchie@hotmail.co.uk>
2025-02-26 23:22:14 +0000

    new pages added, new media added, JS functionality to hid and reveal experience content added

--- code/functions.js
+++ code/functions.js
@@ -1,34 +1,26 @@
 $(document).ready(function(){
     // This function creates a quick fade in when moving between web pages
     $(".page-content").css("display", "none");
-    $(".page-content").fadeIn(500);
+    $(".page-content").fadeIn(1000);
 
     // Creates a double fade out/fade in on the image
     $("#mug-shot").click(function(){
         $("#mug-shot").fadeOut("300");
         $("#mug-shot").fadeIn("300");
-        $("#mug-shot").fadeOut("300");
-        $("#mug-shot").fadeIn("300");
       });
-
-
-    //   if answer to quiz is false, paragraph is revealed and quiz is hidden
-    //   if true, a failure message is revealed
-    $("#quiz").submit(function(event){
-        event.preventDefault();
-        var answer = $('input[name="answer"]:checked').val();
-        if(answer=="false"){
-        $("#blurb").css("color","rgb(202, 202, 202)");
-        $("#blurb-link").css("color","rgb(67, 111, 255)");
-        $("#paragraph").fadeIn("200");
-        $("#wrong").hide();
-        } else {
-            $("#wrong").css("color","rgb(202, 202, 202)");
-        }
-        $("#quiz").hide("slow");
-        $("#quiz-title").hide("slow");
-    })
-
+      
+      $(".experience").hide();
+      $("#ten10").show(); // Default visible experience
+  
+      // Handle clicking on the list items
+      $(".experience-list li").click(function() {
+          // Remove active class from all experiences
+          $(".experience").removeClass('active').hide();
+  
+          // Show the selected experience
+          var experienceId = $(this).data('experience');
+          $("#" + experienceId).addClass('active').fadeIn();
+      });
 
 });
 
