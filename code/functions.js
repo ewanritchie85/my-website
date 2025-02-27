7157debe1ac035cf68dd3a443279f5e4ca9c6aba
EWAN <ewanritchie@hotmail.co.uk>
2025-02-27 22:39:07 +0000

    In NC experience section: tech stack logos added, video removed and architecture image added

--- code/functions.js
+++ code/functions.js
@@ -7,21 +7,18 @@ $(document).ready(function(){
     $("#mug-shot").click(function(){
         $("#mug-shot").fadeOut("300");
         $("#mug-shot").fadeIn("300");
-      });
-      
-      $(".experience").hide();
-      $("#ten10").show(); // Default visible experience
-  
-      // Handle clicking on the list items
-      $(".experience-list li").click(function() {
-          // Remove active class from all experiences
-          $(".experience").removeClass('active').hide();
-  
-          // Show the selected experience
-          var experienceId = $(this).data('experience');
-          $("#" + experienceId).addClass('active').fadeIn();
-      });
+    });
 
+    $(".experience").hide();
+    $("#ten10").show(); // Default visible experience
+
+    // Handle clicking on the list items
+    $(".experience-list li").click(function() {
+        // Remove active class from all experiences
+        $(".experience").removeClass('active').hide();
+
+        // Show the selected experience
+        var experienceId = $(this).data('experience');
+        $("#" + experienceId).addClass('active').fadeIn();
+    });
 });
-
-
